bde62744ebe64d6e1117778b20104cd7a0533a31
atosti <>
2019-07-08 16:10:59 -0700

    API key loaded from txt file

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,16 @@
 import http.client
 
-# FIXME - Move this functionality to a helper method/file later
-vaApi = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo'
+# Fetch your API key from the "avApi.txt" file
+# This file should ONLY contain the API key, and no other text
+f = open('./backend/avApi.txt', "r")
+lines = list(f)
+vaApiKey = ''.join(lines)
+print('Loaded API Key: ' + vaApiKey)
 
-conn = http.client.HTTPSConnection('localhost', 8080)
-h1 = http.client.HTTPConnection('www.python.org')
-# h1
-print('h1 connection established')
+# FIXME - Move this functionality to a helper method/file later
+# avApi = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo'
+
+# conn = http.client.HTTPSConnection('localhost', 8080)
+# h1 = http.client.HTTPConnection('www.python.org')
+# # h1
+# print('h1 connection established')
