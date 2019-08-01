5306c9400aa76428a6e961e0ab2718531f453f80
atosti <>
2019-08-01 13:07:18 -0700

    IntradayTimeSeries calls are now modular.

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,14 @@
 import http.client
 
-# Takes the Stock symbol to fetch and the time interval
+import requests
+
+# TIME_SERIES_INTRADAY API call
 def intradayTimeSeries(symbol, interval, vaApiKey):
-    # https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo
-    avUrl = 'www.alphavantage.co'
-
-    # avUrl = 'www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='
-    # avUrl += symbol + '&interval=' + interval + '&apikey=' + vaApiKey
-    # # FIXME - Remove later
-    # print('Url: ' + avUrl)
-
-    conn = http.client.HTTPSConnection(avUrl, 443, timeout=10)
-    conn.request("GET", "/")
-    response = conn.getresponse()
-    respBody = response.read()
-    print(respBody)
-    conn.close() #Close the connection when done
-    return
+    resp = requests.get('https://www.alphavantage.co/query?'
+    + 'function=TIME_SERIES_INTRADAY&symbol=' + symbol
+    + '&interval=' + interval
+    + '&apikey=' + vaApiKey)
+    return resp
 
 # Fetches your API key from the "avApi.txt" file
 # This file should ONLY contain the API key, no other text/linebreaks
@@ -24,4 +16,8 @@ f = open('./backend/avApi.txt', "r")
 lines = list(f)
 vaApiKey = ''.join(lines)
 
-intradayTimeSeries('MSFT', '5min', vaApiKey)
+# intradayTimeSeries('MSFT', '5min', vaApiKey)
+
+# Make a GET request
+resp = intradayTimeSeries('IP', '5min', vaApiKey)
+print('VA Response: ' + resp.text)
