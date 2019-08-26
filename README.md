a6aa5e6abf106c50c53138bb76a9dc55caa8b697
atosti <>
2019-08-25 23:57:59 -0700

    Added tsd and tsda commands

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 ## Circle of Competence
 A work in progress to analyze stock symbols based on various criteria.
+The aim of this project is to quickly analyze a stock with Value Investing
+criteria, with the aim of determining a rough intrinsic value.
+
+As with all investing tools, use it at your own risk. ;)
 
 ## To Do List (Core features)
-1. Implement python backend for HTTPS calls to Alpha Vantage.
-2. Implement TS frontend to show summary of saved Symbol/Exchange pairs.
-3. Add simple saving of calculated values to a Postgres DB.
-4. Add the ability to save the values to a user profile.
-    * Might be unnecessary at first, consider more general approach.
-5. Place API key in its own file and read it from there (so it's never pushed here).
-6. Create quick setup steps for others to use this repository.
+1. Add the ability to save the values to a local file.
+    * Consider making an xlsx, so the raw data is human readable.
+2. Create quick setup steps for others to use this repository.
+3. Add a 'help' command.
 
 ## Instructions to Run on your system
 1. Clone the repository
 2. Open the `backend` folder and create a new `avApiKey.txt` file.
 3. Inside this file, simply paste your API key on a single line.
 4. Run `main.py` inside the `backend` folder, it should read your API key from the text file automatically.
+
+## Commands (to be moved to the wiki later)
+1. Exit/Quit
+    * Simply exits the program.
+2. Tsd
+    * Makes a Time Series Daily API call. Requires two parameters:
+    A stock symbol, and the time interval.
+    `tsd MSFT 5min`
+3. Tsda
+    * Makes a Time Series Daily Adjusted API call. Requires one parameter:
+    A stock symbol
+    `tsda MSFT`
+4. Help
+    * TBD - Will eventually provide a basic use guide inside the prompt.
--- backend/main.py
+++ backend/main.py
@@ -3,21 +3,67 @@ import http.client
 import requests
 
 # TIME_SERIES_INTRADAY API call
-def intradayTimeSeries(symbol, interval, vaApiKey):
+def timeSeriesDaily(symbol, interval, avApiKey):
     resp = requests.get('https://www.alphavantage.co/query?'
     + 'function=TIME_SERIES_INTRADAY&symbol=' + symbol
     + '&interval=' + interval
-    + '&apikey=' + vaApiKey)
+    + '&apikey=' + avApiKey)
     return resp
 
-# Fetches your API key from the "avApi.txt" file
-# This file should ONLY contain the API key, no other text/linebreaks
+# TIME_SERIES_DAILY_ADJUSTED API call
+def timeSeriesDailyAdjusted(symbol, avApiKey):
+    resp = requests.get('https://www.alphavantage.co/query?'
+    + 'function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + symbol
+    + '&apikey=' + avApiKey)
+    return resp
+
+# Command processing
+def commandHandler(phrase):
+    keywords = phrase.split(' ')
+    cmd = keywords[0] # Gets the command keyword
+
+    if cmd == 'help':
+        print('TBD - List of usable commands here!')
+    elif cmd == 'tsd':
+        if len(keywords) < 3:
+            print('Invalid syntax. Time Series Daily requires a symbol'
+            + ' and interval to be passed.')
+        else:
+            print('Response: '
+            + timeSeriesDaily(keywords[1], keywords[2], avApiKey).text)
+    elif cmd == 'tsda':
+        if len(keywords) < 2:
+            print('Invalid syntax. Time Series Daily Adjusted requires a symbol'
+            + ' to be passed.')
+        else:
+            print('Response: '
+            + timeSeriesDailyAdjusted(keywords[1], avApiKey).text)
+    elif cmd == 'quit' or cmd == 'exit':
+        print('Exiting...')
+    else:
+        print('Unrecognized command: ' + cmd)
+    return
+
+# Fetches your API key from the "avApiKey.txt" file
 f = open('./backend/avApiKey.txt', "r")
 lines = list(f)
 avApiKey = ''.join(lines)
 
-# intradayTimeSeries('MSFT', '5min', vaApiKey)
+# Command loop
+phrase = ''
+print('Enter a command(type \'quit\' or \'exit\' when finished):')
+while (phrase != 'quit' and phrase != 'exit'):
+    print('$ ', end='') # Signals to the user they're in a command prompt
+    phrase = input()
+    phrase = phrase.lower()
+    commandHandler(phrase)
 
-# Make a GET request
-resp = intradayTimeSeries('IP', '5min', avApiKey)
-print('VA Response: ' + resp.text)
+
+# Make a GET request for testing
+# resp = timeSeriesDaily('MSFT', '5min', avApiKey)
+# print('VA Response: ' + resp.text)
+
+# resp2 = timeSeriesDailyAdjusted('IP', avApiKey)
+# print('tsDailyAdjusted: ' + resp2.text)
+
+# TODO - Figure out Annual earnings (historical if possible)
