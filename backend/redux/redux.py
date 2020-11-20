4cb96c109c508bd002fa7d4691ba188a72c04dc3
atosti <>
2020-11-19 23:47:09 -0800

    Input loop no longer mentions NYSE, as any stock symbol is valid.

--- backend/redux/redux.py
+++ backend/redux/redux.py
@@ -1,9 +1,9 @@
 import handler as handler
 
 phrase = ''
-print('Enter a NYSE symbol (type \'quit\' to exit): ')
+print('Enter a stock symbol (type \'quit\' to exit): ')
 while(phrase != 'quit'):
     print('$', end='')
     phrase = input().lower()
-    if phrase != 'quit':
+    if phrase != 'quit' and phrase != '':
         handler.commands(phrase)
