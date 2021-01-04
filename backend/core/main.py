f5cbb87a2d3ee3905baa948d8a8563d6a65506c0
atosti <>
2021-01-04 14:38:11 -0800

    Removed duplicate of main.py, it's now located outside core.

--- backend/core/main.py
+++
@@ -1,9 +0,0 @@
-import handler
-
-phrase = ''
-print('Enter a stock symbol (type \'quit\' to exit): ')
-while(phrase != 'quit'):
-    print('$', end='')
-    phrase = input().lower()
-    if phrase != 'quit' and phrase != '':
-        handler.commands(phrase)
