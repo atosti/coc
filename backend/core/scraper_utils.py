280e415c85a68312435ad3d99f7769d11329e33f
Antonio Tosti <5588048+atosti@users.noreply.github.com>
2022-06-04 15:28:58 -0700

    Scraping requests include a header to prevent being blocked by websites

--- backend/core/scraper_utils.py
+++ backend/core/scraper_utils.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 
 
 def get_soup(url, user="", password=""):
+    headers = requests.utils.default_headers()
+    headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+    })
     if user and password:
-        page = requests.get(url, auth=(user, password))
+        page = requests.get(url, auth=(user, password), headers=headers)
     else:
-        page = requests.get(url)
+        page = requests.get(url, headers=headers)
     return BeautifulSoup(page.content, "html.parser")
 
 
